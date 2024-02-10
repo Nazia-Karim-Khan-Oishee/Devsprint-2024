521573b12ab6c07c1a06b21e7e2b745b2e467f5a
Sadnam Sakib Apurbo <sadnamsakib@iut-dhaka.edu>
2024-02-10 14:19:26 +0600

    React folder structure update

---
+++ client/src/pages/Signup/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const SignupPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    // Perform signup logic here
+    console.log("Signup:", name, email, password);
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleSignup}>
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
